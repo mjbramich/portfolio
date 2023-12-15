adc0fc55d20de3760d1a5c2dc419ea8050a079d8
Matthew Bramich <matt_bramich@hotmail.com>
2023-12-15 13:05:10 +1100

    chore: add ESLint plugins and configurations

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+	`next lint --fix --file ${filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(' --file ')}`;
+
 module.exports = {
-	'*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix', 'eslint'],
+	'*.{js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
 	'**/*.ts?(x)': () => 'npm run check-types',
 	'*.{json,yaml}': ['prettier --write']
 };
