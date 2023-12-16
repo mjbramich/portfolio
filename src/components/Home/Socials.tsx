e99b89e3420f3dcd4542647aeb18c8b5ca21e1ef
Matthew Bramich <matt_bramich@hotmail.com>
2023-12-16 11:07:06 +1100

    feat: implement social component

---
+++ src/components/Home/Socials.tsx
@@ -0,0 +1,44 @@
+import type { FC } from 'react';
+import { FiGithub, FiLinkedin, FiTwitter } from 'react-icons/fi/index';
+import { RxDiscordLogo } from 'react-icons/rx';
+import styles from '@css/home.module.css';
+
+const Socials: FC = () => (
+	<div className={styles.socials}>
+		{[
+			{
+				aria: 'GitHub',
+				icon: FiGithub,
+				href: 'https://github.com/mjbramich'
+			},
+			{
+				aria: 'Twitter',
+				icon: FiTwitter,
+				href: 'https://twitter.com/mjbramich'
+			},
+			{
+				aria: 'Linkedin',
+				icon: FiLinkedin,
+				href: 'https://linkedin.com/in/mjbramich/'
+			},
+			{
+				aria: 'Discord',
+				icon: RxDiscordLogo,
+				href: 'https://discord.com/users/759379629148078121'
+			}
+		].map((link) => (
+			<a
+				href={link.href}
+				target='_blank'
+				rel='noreferrer'
+				className={styles.social}
+				key={link.aria}
+				aria-label={link.aria}
+			>
+				<link.icon size={22} />
+			</a>
+		))}
+	</div>
+);
+
+export default Socials;
