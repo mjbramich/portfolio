341ec4269944d5d8bcc84c6de60028290997fd6b
Matthew Bramich <matt_bramich@hotmail.com>
2023-12-19 10:19:42 +1100

    feat(data): add new entries to projects data file

---
+++ src/components/Projects/data.ts
@@ -0,0 +1,30 @@
+import { Projects } from './Card';
+
+const projects: Projects[] = [
+	{
+		title: "Kanga's Exterior Solutions",
+		img: '/photos/@mjbramich/selfie.jpg',
+		desc: 'A site for a local property maintenance business to showcase their work',
+		links: ['https://kangas.au', 'https://github.com/mjbramich/kangas']
+	},
+	{
+		title: "Kanga's Exterior Solutions",
+		img: '/photos/@mjbramich/selfie.jpg',
+		desc: 'A site for a local property maintenance business to showcase their work',
+		links: ['https://kangas.au', 'https://github.com/mjbramich/kangas']
+	},
+	{
+		title: "Kanga's Exterior Solutions",
+		img: '/photos/@mjbramich/selfie.jpg',
+		desc: 'A site for a local property maintenance business to showcase their work',
+		links: ['https://kangas.au', 'https://github.com/mjbramich/kangas']
+	},
+	{
+		title: "Kanga's Exterior Solutions",
+		img: '/photos/@mjbramich/selfie.jpg',
+		desc: 'A site for a local property maintenance business to showcase their work',
+		links: ['https://kangas.au', 'https://github.com/mjbramich/kangas']
+	}
+];
+
+export default projects;
