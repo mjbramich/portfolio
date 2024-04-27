9fe037df755b9ea9711e0fe26433631103eef243
matt <matt_bramich@hotmail.com>
2024-04-27 16:41:49 +1000

    feat: add new project to work page

--- src/components/Work/data.ts
+++ src/components/Work/data.ts
@@ -22,19 +22,6 @@ const skills: Content = {
 
 const projects: Projects[] = [
 	{
-		title: "Kanga's Exterior Solutions",
-		imgSrc: {
-			sml: '/photos/projects/kangasSmallMockup.png',
-			lrg: '/photos/projects/kangasMockup.png'
-		},
-		desc: 'Designed and developed a website for a local property maintenance business, providing an effective platform to showcase their portfolio of work.',
-		links: {
-			source: 'https://kangas.au',
-			github: 'https://github.com/mjbramich/kangas'
-		},
-		color: blurDataURL(131, 140, 155)
-	},
-	{
 		title: 'Playmaker Vintage (Coming Soon)',
 		imgSrc: {
 			sml: '/photos/projects/playmakerVintage.png',
@@ -46,6 +33,32 @@ const projects: Projects[] = [
 			github: 'https://github.com/mjbramich'
 		},
 		color: blurDataURL(154, 207, 229)
+	},
+	{
+		title: 'Gutter Gladiators',
+		imgSrc: {
+			sml: '/photos/projects/gutterGladiatorsMockupSmall.png',
+			lrg: '/photos/projects/gutterGladiatorsMockup.png'
+		},
+		desc: 'Created a modern website for a gutter cleaning business, showcasing their expertise and reliability. Built with Astro for fast performance and smooth user experience. ',
+		links: {
+			source: 'https://guttergladiators.com.au',
+			github: 'https://github.com/mjbramich/gutter-gladiators'
+		},
+		color: blurDataURL(255, 118, 0)
+	},
+	{
+		title: "Kanga's Exterior Solutions",
+		imgSrc: {
+			sml: '/photos/projects/kangasSmallMockup.png',
+			lrg: '/photos/projects/kangasMockup.png'
+		},
+		desc: 'Designed and developed a website for a local property maintenance business, providing an effective platform to showcase their portfolio of work.',
+		links: {
+			source: 'https://kangas.au',
+			github: 'https://github.com/mjbramich/kangas'
+		},
+		color: blurDataURL(131, 140, 155)
 	},
 	{
 		title: 'Kandoo',
